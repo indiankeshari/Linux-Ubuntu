9f46059050a6794f233eddabf6a86a7b2a8f34d1
Aman Keshari <keshari.smrat@gmail.com>
2022-05-10 15:12:32 +0530

    Initial commit

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Give a Number" << endl;
+    cin >> a;
+    cout << "a = " << a << endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
